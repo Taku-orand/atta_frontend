1efce3bf30943199156000d2cc0a2bc054e7c0f3
Takuya <uoastudent182@gmail.com>
2021-09-11 20:59:47 +0900

    ログイン中かどうか判定できる

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,26 +9,37 @@ export default {
     titleTemplate: '%s - atta_frontend',
     title: 'atta_frontend',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+      },
+      {
+        rel: 'stylesheet',
+        href: '//cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: ['~/plugins/auth0.js'],
+
+  auth0: {
+    domain: 'dev-w0-rzoqk.jp.auth0.com',
+    clientID: '3GyZvQ1eR3GTObkONiIOg3fhuas66vqW',
+  },
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -63,13 +74,12 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
