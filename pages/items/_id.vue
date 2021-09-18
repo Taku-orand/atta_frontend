2908f2eca26cc674f68ea1fd9efc62acef9becc2
Takuya <uoastudent182@gmail.com>
2021-09-18 18:35:41 +0900

    QRCODE生成

--- pages/items/_id.vue
+++ pages/items/_id.vue
@@ -1,18 +1,44 @@
 <template>
-  <v-row>
-    <v-col>
-      <v-card>
-        {{ $route.params.id }}
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card>
+          {{ $route.params.id }}
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-btn @click="make"> QRコード生成 </v-btn>
+    <img v-if="code ? true : false" :src=code>
+  </v-container>
 </template>
 <script>
-  export default{
-    props: ['item'],
-    data(){
-      return {
-      }
+import QRCode from 'qrcode'
+export default {
+  props: ['item'],
+  data() {
+    return {
+      code: "",
+      isMade: false
     }
-  }
+  },
+  computed: {
+    qrcode(){
+      return this.code
+    }
+  },
+  methods: {
+    make() {
+      QRCode.toDataURL('http://localhost:3333/lost-items/'+this.$route.params.id, { width: 100 })
+        .then((code) => {
+          // コード(URLスキーム)が生成されるので、imgタグのsrc=の中に値を入れましょう
+          console.log(code)
+          this.code = code
+          this.isMade = true
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    },
+  },
+}
 </script>
