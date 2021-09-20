258ddff10b6c2a31295ec277a77ea21e61123c40
Takuya <uoastudent182@gmail.com>
2021-09-20 16:48:24 +0900

    詳細ページで名前と内容の表示、QRコードを作成し表示する

--- pages/items/_id.vue
+++ pages/items/_id.vue
@@ -1,40 +1,69 @@
 <template>
   <v-container>
-    <v-row>
+    <!-- アイテム情報 -->
+    <v-row class="justify-center">
       <v-col>
         <v-card>
-          {{ $route.params.id }}
+          名前：{{itemName}}<br/>
+          内容：{{itemContent}}
         </v-card>
       </v-col>
+    </v-row>
+
+    <!-- QRコード生成ボタン -->
+    <v-row class="justify-end">
+      <v-col cols="8">
+        <v-text-field v-model="wid"></v-text-field>
+      </v-col>
+      <v-col cols="4">
+        <v-btn class="mr-5" @click="createQRCode(wid)">QRCodeを作成</v-btn>
+      </v-col>
+    </v-row>
+
+    <!-- QRCODE -->
+    <v-row class="justify-center">
+      <div v-if="isCreated"><img :src="qrCode" /></div>
     </v-row>
   </v-container>
 </template>
 <script>
 import QRCode from 'qrcode'
 export default {
-  props: ['item'],
   data() {
     return {
-      code: "",
+      qrCode: '',
+      isCreated: false,
+      wid: 100,
+      itemName: '',
+      itemContent: ''
     }
   },
-  mounted(){
+  mounted() {
     this.$store.commit('isLoggedIn')
+    this.$store.dispatch('item/getItem',this.$route.params.id)
+    this.getItemFromStore()
   },
   methods: {
-    createQRCode() {
+    getItemFromStore(){
+      const Item = this.$store.getters['item/itemGetter']
+      this.itemName = Item.name
+      this.itemContent = Item.content
+    },
+    createQRCode(wid) {
       QRCode.toDataURL(
         `http://localhost:3333/lost-items/${this.$route.params.id}`,
-        { width: 300 }
+        { width: wid }
       )
         .then((qrCode) => {
           // QRcodeをimgタグのsrcに入れる
           this.qrCode = qrCode
+          this.isCreated = true
+          console.log(this.qrCode)
         })
         .catch((err) => {
           console.error(err)
         })
     },
-  }
+  },
 }
 </script>
--- store/item.js
+++ store/item.js
@@ -2,15 +2,21 @@ import axios from 'axios'
 
 export const state = () => ({
   items: [],
+  item: {},
 })
 
 export const getters = {
-  
+  itemGetter(state) {
+    return state.item
+  },
 }
 
 export const mutations = {
   setItems(state, items) {
     state.items = items
+  },
+  setItem(state, item) {
+    state.item = item
   },
 }
 
@@ -42,5 +48,27 @@ export const actions = {
       headers: { Authorization: 'Bearer ' + this.$auth0.getIdToken() },
       withCredentials: true,
     })
+  },
+  getItem({ commit }, itemId) {
+    axios
+      .get(
+        `http://localhost:3000/api/v1/items/${itemId}`,
+
+        {
+          headers: { Authorization: 'Bearer ' + this.$auth0.getIdToken() },
+          withCredentials: true,
+        }
+      )
+      .then((response) => {
+        if (response.data.found) {
+          console.log('見つかりました。')
+          commit('setItem', response.data.item)
+        } else {
+          console.log('見つかりませんでした。')
+        }
+      })
+      .catch((e) => {
+        console.log(e)
+      })
   },
 }
