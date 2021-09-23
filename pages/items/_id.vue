0e8a0aaac092e63b0255721009bc3d8fe3843b96
Takuya <uoastudent182@gmail.com>
2021-09-24 00:58:24 +0900

    編集できる

--- pages/items/_id.vue
+++ pages/items/_id.vue
@@ -4,8 +4,46 @@
     <v-row class="justify-center">
       <v-col>
         <v-card>
-          名前：{{ item.name }}<br />
-          内容：{{ item.content }}
+          <div v-if="!edit">
+            名前：{{ item.name }}<br />
+            内容：{{ item.content }}<br />
+          </div>
+          <div v-if="edit">
+            <v-row dense>
+              <v-col cols="3"> 名前： </v-col>
+              <v-col cols="8">
+                <v-text-field
+                  v-model="tempName"
+                  dense
+                  height="17"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            内容：
+            <v-textarea
+              v-model="tempContent"
+              class="centered-input"
+              :hide-details="true"
+              auto-grow
+              rows="1"
+              outlined
+            />
+          </div>
+          <div v-if="!edit">
+            <v-btn small @click="editItem">編集</v-btn>
+          </div>
+          <div v-if="edit">
+            <v-row row justify-end align-end>
+              <v-col class="justify-center">
+                <v-row>
+                  <v-col>
+                    <v-btn small @click="updateItem">保存</v-btn>
+                    <v-btn small @click="doneEdit">キャンセル</v-btn>
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </div>
         </v-card>
       </v-col>
     </v-row>
@@ -13,7 +51,7 @@
     <!-- QRコード生成ボタン -->
     <v-row class="justify-end">
       <v-col cols="8">
-        <v-text-field v-model="wid"></v-text-field>
+        <v-text-field v-model="wid" label="サイズ" height="17"></v-text-field>
       </v-col>
       <v-col cols="4">
         <v-btn class="mr-5" @click="createQRCode(wid)">QRCodeを作成</v-btn>
@@ -37,6 +75,9 @@ export default {
       qrCode: '',
       isCreated: false,
       wid: 100,
+      edit: false,
+      tempName: '',
+      tempContent: '',
     }
   },
   async fetch({ store, params }) {
@@ -54,6 +95,23 @@ export default {
   },
 
   methods: {
+    async updateItem() {
+      await this.$store.dispatch('item/updateItem', {
+        itemId: this.$route.params.id,
+        item: { name: this.tempName, content: this.tempContent },
+      })
+      this.doneEdit()
+    },
+    editItem() {
+      this.tempName = this.item.name
+      this.tempContent = this.item.content
+      this.edit = true
+    },
+    doneEdit() {
+      this.tempName = ''
+      this.tempContent = ''
+      this.edit = false
+    },
     createQRCode(wid) {
       QRCode.toDataURL(
         `http://localhost:3333/lost-items/${this.$route.params.id}/${this._uid}`,
@@ -64,7 +122,11 @@ export default {
           this.qrCode = qrCode
           await this.$store.dispatch('item/updateItem', {
             itemId: this.$route.params.id,
-            item: { qr_code: this.qrCode, isValid: true, verification_id: this._uid },
+            item: {
+              qr_code: this.qrCode,
+              isValid: true,
+              verification_id: this._uid,
+            },
           })
           await this.$store.dispatch('item/getItem', this.$route.params.id)
         })
--- store/item.js
+++ store/item.js
@@ -73,7 +73,7 @@ export const actions = {
         }
       )
       if (Item.updated) {
-        // 成功
+        commit('setItem', Item.item)
       }
     } catch (e) {
       console.log(e)
