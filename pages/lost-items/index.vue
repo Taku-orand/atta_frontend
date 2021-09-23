1100888b61578753a35c78ff7c5592b66b17db7e
Takuya <uoastudent182@gmail.com>
2021-09-23 13:21:59 +0900

    現在地からマップ表示

--- pages/lost-items/index.vue
+++ pages/lost-items/index.vue
@@ -11,25 +11,74 @@
     </v-row>
     <v-form>
       <v-row row justify-center align-center>
-        <v-col cols="12" class="d-flex justify-center">
-          <v-text-field
-            v-model="foundLocation"
-            label="見つけた場所"
-            class="centered-input"
-            :hide-details="true"
-            placeholder="渋谷のマクドナルド..."
-          />
+        <v-col cols="12" class="justify-center">
+          <ValidationProvider
+            v-slot="{ errors }"
+            name="見つけた場所"
+            rules="required"
+          >
+            <v-text-field
+              v-model="foundLocation"
+              label="見つけた場所"
+              class="centered-input"
+              clearable
+              placeholder="渋谷のマクドナルド..."
+              autofocus
+              :hint="foundLocation"
+              persistent-hint
+              required
+              :error-messages="errors"
+            />
+          </ValidationProvider>
         </v-col>
-        <v-col cols="12" class="d-flex justify-center">
-          <v-text-field
-            v-model="itemDestination"
-            label="落とし物の現在地"
-            class="centered-input"
-            :hide-details="true"
-            placeholder="〇〇の交番、見つけた場所にある"
-          />
+        <v-row justify="end">
+          <v-btn @click="getCurrentAddress"
+            ><v-icon left>mdi-account-arrow-left</v-icon
+            >現在地から入力する</v-btn
+          >
+        </v-row>
+        <v-col cols="12" class="justify-center">
+          <ValidationProvider
+            v-slot="{ errors }"
+            name="見つけた場所"
+            rules="required"
+          >
+            <v-text-field
+              v-model="itemDestination"
+              label="落とし物の現在地"
+              class="centered-input"
+              clearable
+              placeholder="〇〇の交番、見つけた場所にある"
+              :hint="itemDestination"
+              persistent-hint
+              required
+              :error-messages="errors"
+            />
+          </ValidationProvider>
         </v-col>
       </v-row>
+    </v-form>
+    <v-row justify="end">
+      <v-btn @click="sameWithCurrentAddress"
+        ><v-icon left>mdi-account-arrow-left</v-icon>見つけた場所に同じ</v-btn
+      >
+    </v-row>
+    <v-row row justify-center align-center>
+      <v-col cols="12" class="d-flex justify-center">
+        <v-textarea
+          v-model="itemDestinationDetails"
+          label="落とし物の詳細位置情報"
+          class="centered-input"
+          :hide-details="true"
+          auto-grow
+          clearable
+          rows="3"
+          outlined
+          placeholder="マクドの２階の窓側席... 、〇〇の近くにあるよ"
+        />
+      </v-col>
+    </v-row>
+    <v-form v-if="showMap">
       <v-row justify="center" align="center">
         <v-col cols="12" sm="8" md="6">
           <v-card>
@@ -51,12 +100,14 @@
           </v-card>
         </v-col>
       </v-row>
-      <v-row justify="end">
-        <v-btn @click="postNotification"
+    </v-form>
+    <v-row justify="center">
+      <v-col cols="6">
+        <v-btn color="primary" @click="postNotification"
           ><v-icon left>mdi-account-arrow-left</v-icon>持ち主に知らせる!</v-btn
         >
-      </v-row>
-    </v-form>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 <script>
@@ -75,10 +126,12 @@ export default {
         streetViewControl: false,
         styles: [],
       },
-      marker:{
+      marker: {
         title: '忘れ物場所',
         position: { lat: 0, lng: 0 },
-      }
+      },
+      showMap: false,
+      itemDestinationDetails: '',
     }
   },
   async fetch({ store }) {
@@ -105,16 +158,33 @@ export default {
     this.$store.commit('setShowUserInfo', false)
   },
   methods: {
+    async getCurrentAddress() {
+      let requestURL =
+        'https://maps.googleapis.com/maps/api/geocode/json?language=ja&sensor=false'
+      requestURL +=
+        '&latlng=' + this.maplocation.lat + ',' + this.maplocation.lng
+      requestURL += '&key=AIzaSyBXCM7ykgOWUPxDj29CVftzir5yC_sAQyQ'
+      // + process.env.VUE_APP_GOOGLE_API_KEY
+      const Response = await this.$axios.$get(requestURL)
+      // console.log(Response)
+      const FormattedAddress = Response.results[0].formatted_address
+      this.foundLocation = FormattedAddress
+      this.showMap = true
+    },
     getCurrentPosition() {
       return new Promise(function (resolve, reject) {
         navigator.geolocation.getCurrentPosition(resolve, reject)
       })
+    },
+    sameWithCurrentAddress() {
+      this.itemDestination = this.foundLocation
     },
     postNotification() {
       this.$store.dispatch('lost_item/postNotification', {
         lostItemData: {
           foundLocation: this.foundLocation,
           itemDestination: this.itemDestination,
+          itemDestinationDetails: this.itemDestinationDetails,
           email: this.lost_item.user.email,
           user_name: this.lost_item.user.name,
           item_name: this.lost_item.item.name,
